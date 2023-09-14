a798905d5a58c9b745812dd4622d07419a994223
Lucia Aydin <0itslucia0@gmail.com>
2023-09-14 18:35:13 +0300

    Add Copy functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,7 @@ const Demo = () => {
   });
 
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState('');
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
@@ -37,6 +38,12 @@ const Demo = () => {
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
+  };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
@@ -79,9 +86,9 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className="link_card"
             >
-              <div className="copy_btn">
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt="copy icon"
                   className="w-[40%] h-[40%] object-contain"
                 />
